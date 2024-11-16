b9eb9906179d73c2c67e458049c166ff184dd69f
TaeHyun Kim <taehyun.kim1982@gmail.com>
2024-11-16 16:02:09 +0900

    카운트 및 프린트 수정.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ fn main() -> io::Result<()> {
     // 원본 디렉토리 설정 (예: ./pdfs)
     let source_dir = current_dir.join("./Downloads/_DONE/_Magazines");
 
+    // 이동된 파일 개수를 저장할 변수
+    let mut moved_count = 0;
+
     // 원본 디렉토리에서 PDF 파일을 읽어옴
     for entry in fs::read_dir(&source_dir)? {
         let entry = entry?;
@@ -41,11 +44,16 @@ fn main() -> io::Result<()> {
                 // 파일을 지정된 경로로 이동
                 let dest_file = dest_dir.join(entry.file_name());
                 fs::rename(&path, &dest_file)?;
-                println!("Moved {:?} to {:?}\n", file_name, dest_dir);
+                println!("Moved {:?} \n   to {:?}\n", file_name, dest_dir);
+
+                // 이동 성공 시 카운트 증가
+                moved_count += 1;
             }
         }
     }
 
-    println!("PDF 파일 이동이 완료되었습니다.");
+    // 전체 이동된 파일 개수 출력
+    println!("총 이동된 PDF 파일 개수: {}", moved_count);
+
     Ok(())
 }
